32eb78de939889c06af1f01bfab8d37577b0bbe3
Peter Haddad <phadd062@uottawa.ca>
2026-03-26 21:07:26 -0400

    commands to push docker image to ecr

---
+++ push_image.sh
@@ -0,0 +1,4 @@
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 467091806900.dkr.ecr.us-east-1.amazonaws.com
+docker build -t trading-platform-app .
+docker tag trading-platform-app:latest 467091806900.dkr.ecr.us-east-1.amazonaws.com/trading-platform_ecr:latest
+docker push 467091806900.dkr.ecr.us-east-1.amazonaws.com/trading-platform_ecr:latest
